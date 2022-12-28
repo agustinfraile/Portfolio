3dbee12920c77db61f1d0a9e48b2ac404d59d0b4
Agustin Fraile <agustinfraile98@gmail.com>
2022-12-28 19:31:23 -0300

    agregando introduccion

--- src/pages/Intro/Intro.jsx
+++ src/pages/Intro/Intro.jsx
@@ -5,10 +5,17 @@ const Intro = () => {
   return (
     <div className='intro_cnt'>
         <div className='first_cnt'>
-
+            <div className='first_cnt-img'>
+               <div className='first_cnt-img--inside'></div> 
+            </div>
         </div>
         <div className='sec_cnt'>
-
+            <div className='sec_cnt-text'>
+                <h1>Hola! soy Agustin</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+            </div>
         </div>
     </div>
   )
