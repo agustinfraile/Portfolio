1c3b9fecebb5797692aff5ade09d299526803449
Agustin Fraile <agustinfraile98@gmail.com>
2022-12-28 20:07:44 -0300

    agregando componente about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,7 +3,24 @@ import './About.css';
 
 const About = () => {
   return (
-    <div>HOLA About</div>
+    <div className='about-cnt'>
+      <div className='about-cnt-first'>
+        <div className='about-cnt-text'>
+          <h2>Titulo</h2>
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </div>
+      <div className='about-cnt-sec'>
+        <div className='about-cnt-text'>
+          <h2>Titulo</h2>
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
