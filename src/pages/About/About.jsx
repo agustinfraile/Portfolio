5a6dfabe8503cf4c77478c27e11ee526af6c8f7b
Agustin Fraile <agustinfraile98@gmail.com>
2022-12-30 20:45:02 -0300

    creando mediaquieries para la seccion about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,22 +4,29 @@ import './About.css';
 const About = () => {
   return (
     <div className='about-cnt'>
+
       <div className='about-cnt-first'>
+
         <div className='about-cnt-text'>
           <h2>Titulo</h2>
           <p>
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </p>
         </div>
+
       </div>
+
       <div className='about-cnt-sec'>
+        
         <div className='about-cnt-text'>
           <h2>Titulo</h2>
           <p>
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </p>
         </div>
+
       </div>
+      
     </div>
   )
 }
