0bfb0d38fe84c5e08e54213b23df0fe08e287201
Agustin Fraile <agustinfraile98@gmail.com>
2023-01-12 15:16:19 -0300

    agregando iconos de formacion

--- client/src/components/IconEducation/IconEducation.jsx
+++ client/src/components/IconEducation/IconEducation.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './IconEducation.css';
 
-const IconEducation = () => {
+const IconEducation = ({img}) => {
   return (
     <div className='education-cnt'>
+        <img 
+          src={ img } 
+          alt={`Imagen de ${img}`} 
+          title={`Imagen de ${img}`}
+        />
     </div>
   )
 }
--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -8,6 +8,11 @@ import jsLogo from '../../assets/images/icons/tech-logos/js.png'
 import nodeLogo from '../../assets/images/icons/tech-logos/node.png'
 import expressLogo from '../../assets/images/icons/tech-logos/express.png'
 import sqlLogo from '../../assets/images/icons/tech-logos/sql.png'
+import coderLogo from '../../assets/images/icons/formacion-logos/coder.png'
+import henryLogo from '../../assets/images/icons/formacion-logos/henry.png'
+import utnLogo from '../../assets/images/icons/formacion-logos/utn.png'
+import udemyLogo from '../../assets/images/icons/formacion-logos/udemy.png'
+
 
 import './About.css';
 
@@ -48,10 +53,11 @@ const About = () => {
         >
           <h2>Formacion</h2>
           <div className='about-cnt-education'>
-            <IconEducation />
-            <IconEducation />
-            <IconEducation />
-            <IconEducation />
+            <IconEducation img={ coderLogo } />
+            <IconEducation img={ henryLogo } />
+            <IconEducation img={ utnLogo } />
+            <IconEducation img={ udemyLogo } />
+
           </div>
         </div>
 
